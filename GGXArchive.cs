16e7c4743f50c11179f61d605858b86c68ce4b15
byte-chan™ <baumi@bytesoft.dev>
2025-09-17 00:29:53 +0200

    GGXArchive: Accept encoding instead of codepage

--- GGXArchive.cs
+++ GGXArchive.cs
@@ -73,7 +73,7 @@ public readonly struct ArchiveFile(GGXCompression compression, byte[] data, int 
 	}
 }
 
-public class GGXArchive(int codepage = 932, bool uppercase = true, char separator = '\\') {
+public class GGXArchive(Encoding? encoding = null, bool uppercase = true, char separator = '\\') {
 	public static readonly byte[] xorData = [
 		0x28, 0x43, 0x29, 0x32, 0x30, 0x30, 0x37, 0x47, 0x55, 0x4C, 0x54, 0x49, 0x43, 0x4F, 0x20, 0x20, // "(C)2007GULTICO  "
 		0x47, 0x47, 0x58, 0x20, 0x41, 0x72, 0x63, 0x68, 0x69, 0x76, 0x65, 0x20, 0x20, 0x20, 0x20, 0x20, // "GGX Archive     "
@@ -93,7 +93,7 @@ public class GGXArchive(int codepage = 932, bool uppercase = true, char separato
 		0x5B, 0x3D, 0x7E, 0x25, 0x28, 0x26, 0x21, 0x2D, 0x23, 0x5E, 0x40, 0x29, 0x5D, 0x27, 0x7C, 0x24, // "[=~%(&!-#^@)]'|$"
 	];
 	static readonly byte[] magic = "GGXArchiver1.00\0"u8.ToArray();
-	readonly Encoding encoding = Encoding.GetEncoding(codepage);
+	readonly Encoding encoding = encoding ?? Encoding.GetEncoding(932);
 	readonly Dictionary<string, ArchiveFile> files = [];
 	public IEnumerable<string> Files => files.Keys;
 	public void Add(string path, ArchiveFile entry) {
